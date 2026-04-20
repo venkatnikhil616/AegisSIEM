a43245211db77c290d186d20eca9ceef0d47f93e
Venkat Nikhil Vallamsetty <venkatnikhil616@gmail.com>
2026-04-20 14:57:03 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,7 @@
+class Config:
+    # For your current SIMULATION mode, interface is NOT actually used
+    # but your code still requires a value to pass validation
+
+    INTERFACE = "lo"   # loopback (safe default)
+
+    DEBUG = True
